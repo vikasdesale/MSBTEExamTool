ec27f05d34d5bf482b7227fe22240339ccbb18c3
vikasdesale <vikas.desaleu@gmail.com>
2017-03-04 23:10:06 +0530

    1.1_toolbar_added_to_activity

--- app/src/main/java/com/kbh/msbteexamtool/activity/MainActivity.java
+++ app/src/main/java/com/kbh/msbteexamtool/activity/MainActivity.java
@@ -5,10 +5,15 @@ package com.kbh.msbteexamtool.activity;
  */
 
  import android.os.Bundle;
+ import android.support.design.widget.CollapsingToolbarLayout;
+ import android.support.design.widget.TabLayout;
+ import android.support.v4.view.ViewPager;
  import android.support.v7.app.AppCompatActivity;
  import android.support.v7.widget.Toolbar;
  import android.view.Menu;
  import android.view.MenuItem;
+ import android.widget.ImageView;
+
  import com.kbh.msbteexamtool.R;
 
 
@@ -18,11 +23,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.htab_toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Parallax Tabs");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.htab_viewpager);
+        //setupViewPager(viewPager);
+
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.htab_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        final CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.htab_collapse_toolbar);
+        collapsingToolbarLayout.setTitleEnabled(false);
 
 
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
